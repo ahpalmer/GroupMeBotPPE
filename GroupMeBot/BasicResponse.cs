cf6f228df1c5125921653f12ae79a05291dd7d4c
Andrew Palmer <ahpalmer72@gmail.com>
2024-02-04 20:34:04 -0500

    Cleanup commit

--- GroupMeBot/BasicResponse.cs
+++ GroupMeBot/BasicResponse.cs
@@ -6,7 +6,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using Newtonsoft;
 using GroupMeBot.Model;
 
 namespace GroupMeBot.Controller;
