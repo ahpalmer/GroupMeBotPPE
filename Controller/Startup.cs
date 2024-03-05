9485c97240de84e1171c94c7a33732ed9ee9d0d9
Andrew Palmer <ahpalmer72@gmail.com>
2024-03-04 22:57:21 -0500

    Added a todo comment to solve the constructor injection problem for my DI

--- Controller/Startup.cs
+++ Controller/Startup.cs
@@ -13,10 +13,10 @@ public class Startup : FunctionsStartup
     public override void Configure(IFunctionsHostBuilder builder)
     {
         builder.Services.AddHttpClient();
+        // Todo: create a IMessageIncoming factory instead of the direct class so that you can inject the httprequest into the IMessageIncoming class
         builder.Services.AddSingleton<IMessageIncoming, MessageIncoming>();
         builder.Services.AddSingleton<IMessageOutgoing, MessageOutgoing>();
         builder.Services.AddSingleton<IAnalysisBot, AnalysisBot>();
         builder.Services.AddSingleton<IMessageBot, MessageBot>();
-
     }
 }
